75cfae30c73bb50eaa48efe3ede161552bc735fb
Lucian Mocanu <alumni@users.noreply.github.com>
2024-12-01 22:56:02 +0100

    Solve day 1

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,9 +1,14 @@
 import { runSolution } from '../utils.ts';
+import { isEmpty, unzip, zip } from 'radashi';
 
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+    const [a, b] = unzip(
+        data.filter((line) => !isEmpty(line)).map((line) => line.split(/\s+/).map(Number) as [number, number]),
+    );
+    const sortedData = zip(a.sort(), b.sort());
+
+    return sortedData.reduce((sum, [a, b]) => sum + Math.abs(a - b), 0);
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -1,9 +1,15 @@
+import { counting, isEmpty, unzip } from 'radashi';
 import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+    const [a, b] = unzip(
+        data.filter((line) => !isEmpty(line)).map((line) => line.split(/\s+/).map(Number) as [number, number]),
+    );
+
+    const counts = counting(b, (value) => value);
+
+    return a.reduce((sum, value) => sum + value * (counts[value] ?? 0), 0);
 }
 
 await runSolution(day1b);
