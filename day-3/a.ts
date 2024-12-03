7c52a3579dafc6a352e72fe34dc154ab01a4ffeb
Lucian Mocanu <alumni@users.noreply.github.com>
2024-12-03 23:34:54 +0100

    Solve day 3

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,14 @@
 import { runSolution } from '../utils.ts';
 
+const FORMULA_REGEX = /mul\((\d+),(\d+)\)/g;
+
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+    const text = data.join('\n');
+
+    return [...text.matchAll(FORMULA_REGEX)].reduce((sum, match) => {
+        return sum + Number(match[1]) * Number(match[2]);
+    }, 0);
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -1,9 +1,32 @@
 import { runSolution } from '../utils.ts';
 
+const FORMULA_REGEX = /mul\((\d+),(\d+)\)/g;
+
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+    const text = data.join('\n');
+    let sum = 0;
+    let position = (FORMULA_REGEX.lastIndex = 0);
+
+    while (position < text.length) {
+        let stop = text.indexOf("don't()", FORMULA_REGEX.lastIndex);
+        if (stop < 0) stop = text.length;
+
+        while (FORMULA_REGEX.lastIndex <= stop) {
+            const match = FORMULA_REGEX.exec(text);
+            if (!match) {
+                position = text.length;
+                break;
+            }
+            if (FORMULA_REGEX.lastIndex <= stop) {
+                sum = sum + Number(match[1]) * Number(match[2]);
+            }
+        }
+        position = FORMULA_REGEX.lastIndex = text.indexOf('do()', stop);
+        if (position < 0) break;
+    }
+
+    return sum;
 }
 
 await runSolution(day3b);
