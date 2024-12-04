8315300f13fd36280fbd56f212d11bff2184e35a
Lucian Mocanu <alumni@users.noreply.github.com>
2024-12-04 22:41:23 +0100

    Solve day 4

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,42 @@
+import { isEmpty, list } from 'radashi';
 import { runSolution } from '../utils.ts';
+
+const INCREMENTS = [
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+];
+const SEARCH = 'XMAS';
 
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+    const a = data.filter((line) => !isEmpty(line));
+    let count = 0;
+
+    const get = (i, j) => {
+        const row = i >= 0 ? a.at(i) : null;
+        const cell = j >= 0 ? row?.at(j) : null;
+
+        return cell ?? '';
+    };
+
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a[i].length; j++) {
+            for (const [di, dj] of INCREMENTS) {
+                const word = list(0, 3, (k) => get(i + k * di, j + k * dj)).join('');
+                if (word === SEARCH) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,31 @@
+import { isEmpty } from 'radashi';
 import { runSolution } from '../utils.ts';
+
+const PATTERN_POSITIONS = [
+    [0, 0],
+    [0, 2],
+    [1, 1],
+    [2, 0],
+    [2, 2],
+];
+
+const VALID_PATTERNS = ['SSAMM', 'MMASS', 'MSAMS', 'SMASM'];
 
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+    const a = data.filter((line) => !isEmpty(line));
+    let count = 0;
+
+    for (let i = 0; i < a.length - 2; i++) {
+        for (let j = 0; j < a[i].length - 2; j++) {
+            const pattern = PATTERN_POSITIONS.map(([di, dj]) => a[i + di][j + dj]).join('');
+            if (VALID_PATTERNS.includes(pattern)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
 
 await runSolution(day4b);
